fac26f464bf6de777a4e67985627c336aba2c80d
Clara Martins <up201806528@fe.up.pt>
2020-11-21 14:49:49 +0000

    move/3

--- game.pl
+++ game.pl
@@ -1,4 +1,36 @@
 
 :- include('board.pl').
-
+/**
+ * Game State = Dimensions-Board
+ */
 talpa(Dimensions, RedBot-BlueBot).
+
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------                Movement                --------------------
+ --------------------------------------------------------------------------------
+**/
+/**
+ * Moves a Piece
+ * GameState = Dimensions-Board-Player
+ * Movement:
+ *      x - 120
+ *      u - 
+ */
+% move(+GameState, +Move, -NewGameState)
+move(Dimensions-Board-PlayerOnMove, Column-Line-Movement, Dimensions-NewBoard-NextPlayer) :-
+    Movement \= x,
+    replace_on_board(Column-Line, ' ', Board, NewBoard),
+    movement_position(Column-Line, Movement, NextColumn-NextLine),
+    player_symbol(PlayerOnMove, Element),
+    replace_on_board(NextColumn-NewLine, Element, Board, NewBoard),
+    NextPlayer is -Player.
+
+move(Dimensions-Board-PlayerOnMove, Column-Line-120, Dimensions-NewBoard-NextPlayer) :-
+    replace_on_board(Column-Line, ' ', Board, NewBoard),
+    NextPlayer is -Player.
+
+movement_position(Column-Line, 120, NextColumn-NextLine).
+    
+
--- utils.pl
+++ utils.pl
@@ -6,3 +6,23 @@
 % player_symbol(+Player, -Symbol)
 player_symbol(+ 1, 'X').
 player_symbol(- 1, 'O').
+
+/**
+ * Replace element in Cell for NewElement
+ */
+% replace_on_board(+Cell, +NewElement, +Board, -NewBoard)
+replace_on_board(Column-Line, Element, Board, NewBoard).
+
+replace(Column-Line, C-L, Element, Board, NewBoard) :-
+    L > Line,
+    NewL is L - 1,
+    replace(Column-Line, C-NewL, Element, Board, NewBoard).
+
+replace(Column-Line, C-Line, Element, Board, NewBoard) :-
+    C < Column,
+    NewC is C + 1,
+    replace(Column-Line, NewC-Line, Element, Board, NewBoard).
+
+replace(Column-Line, Column-Line, Element, Board, NewBoard).
+
+
