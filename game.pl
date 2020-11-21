409a89228c3009b1db8a3b26b49c4064339c7b08
Clara Martins <up201806528@fe.up.pt>
2020-11-21 14:50:05 +0000

    game_over/2

--- game.pl
+++ game.pl
@@ -34,3 +34,60 @@ move(Dimensions-Board-PlayerOnMove, Column-Line-120, Dimensions-NewBoard-NextPla
 movement_position(Column-Line, 120, NextColumn-NextLine).
     
 
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------                Game Over               --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
+ * Decides the Winner of the Game based on the Current GameState
+ * In this particular function,
+ * GameState = Dimensions-Board-PlayerOnMove
+ * 
+ * Winner:
+ *        0 -> not gameover yet
+ *        1 -> red  player wins
+ *      - 1 -> blue player wins
+ */
+% game_over(+GameState, -Winner)
+game_over(Dimensions-Board-PlayerOnMove, Winner) :-
+    find_path_blue_blue(Dimensions-Board, BluePath),
+    find_path_red_red(Dimensions-Board, RedPath),
+    find_winner(BluePath-RedPath, PlayerOnMove, Winner).
+
+/**
+ * Choose the Winner based on the Existing Path Between the Edges
+ */
+% find_winner(+Paths,+PlayerOnMove, -Winner)
+find_winner(0-0, _, 0).
+find_winner(1-0, _, -1).
+find_winner(0-1, _, +1).
+find_winner(1-1, Player, Winner) :- Winner is -Player. % TODO : see if i can make (1-1, Player, -Player)
+
+/**
+ * Checks if a path exists between the red edges (up and down)
+ * Return Values:
+ *          0 - no path
+ *          1 - path
+ */
+% find_path_red_red(+GameState, -Path)
+find_path_red_red(Dimensions-Board, 1) :-
+    find_red_paths(Dimensions-Board).
+find_path_red_red(Dimensions-Board, 0).
+
+find_red_paths(Dimensions-Board).
+
+/**
+ * Checks if a path exists between the blue edges (left and right)
+ * Return Values:
+ *          0 - no path
+ *          1 - path
+ */
+% find_path_blue_blue(+GameState, -Path)
+find_path_blue_blue(Dimensions-Board, 1) :-
+    find_blue_paths(Dimensions-Board).
+find_path_blue_blue(Dimensions-Board, 0).
+
+find_blue_paths(Dimensions-Board).
