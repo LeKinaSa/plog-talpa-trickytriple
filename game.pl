8e46fab7de104988a4f7a5c98258eb9ebbcc02e3
Clara Martins <up201806528@fe.up.pt>
2020-11-22 15:45:27 +0000

    replace, inside_board, board_cell, adjacent_position, adjacent_cell,  path_finder now in utils
    movement_position -> adjacent_position

--- game.pl
+++ game.pl
@@ -1,14 +1,21 @@
-
 :- include('board.pl').
+
 /**
  * Game State = Dimensions-Board
  */
 talpa(Dimensions, RedBot-BlueBot).
 
+/**
+ --------------------------------------------------------------------------------
+ --------------------               Valid Moves              --------------------
+ --------------------------------------------------------------------------------
+**/
+% valid_moves(+GameState, +Player, -ListOfMoves)
+valid_moves(Dimensions-Board-Player, _, PossibleMoves).
 
 /**
  --------------------------------------------------------------------------------
- --------------------                Movement                --------------------
+ --------------------              Move a Piece              --------------------
  --------------------------------------------------------------------------------
 **/
 /**
@@ -22,7 +29,7 @@ talpa(Dimensions, RedBot-BlueBot).
 move(Dimensions-Board-Player, Column-Line-Movement, Dimensions-NewBoard-NextPlayer) :-
     Movement \= x,
     replace(Column-Line, ' ', Dimensions-Board, AuxBoard),
-    movement_position(Column-Line, Movement, NextColumn-NextLine),
+    adjacent_position(Column-Line, Movement, NextColumn-NextLine),
     player_symbol(Player, Element),
     replace(NextColumn-NextLine, Element, Dimensions-AuxBoard, NewBoard),
     NextPlayer is -Player.
@@ -30,63 +37,6 @@ move(Dimensions-Board-Player, Column-Line-Movement, Dimensions-NewBoard-NextPlay
 move(Dimensions-Board-Player, Column-Line-x, Dimensions-NewBoard-NextPlayer) :-
     replace(Column-Line, ' ', Dimensions-Board, NewBoard),
     NextPlayer is -Player.
-
-/**
- * Calculates the cell to where the piece is going to be moved to
- * Movement:
- *      Up    : u
- *      Down  : d
- *      Left  : l
- *      Right : r
- */
-% movement_position(+Cell, +Movement, -NewCell)
-movement_position(Column-Line, u, Column-NextLine) :-
-    NextLine is Line + 1.
-
-movement_position(Column-Line, d, Column-NextLine) :-
-    NextLine is Line - 1.
-
-movement_position(Column-Line, l, NextColumn-Line) :-
-    NextColumn is Column - 1.
-
-movement_position(Column-Line, r, NextColumn-Line) :-
-    NextColumn is Column + 1.
-
-/**
- * Replace the Cell Value in Board for Element
- * BoardInfo = Dimensions-Board
- * In the board, Column goes from      1     to Dimensions
- *           and  Line  goes from Dimensions to      1
- */
-% replace(+Cell, +Element, +BoardInfo, -NewBoard)
-replace(Column-Line, Element, Dimensions-Board, NewBoard) :-
-    replace_on_board(Column-Line, Dimensions, Element, Board, NewBoard).
-
-/**
- * Replaces the TargetCell value for Element
- * Helper Function for replace/4
- */
-% replace_on_board(+TargetCell, +CurrentLine, +Element, +Board, -NewBoard)
-
-replace_on_board(TargetColumn-TargetLine, CurrentLine, Element, [Line | Board], [Line | NewBoard]) :-
-    CurrentLine > TargetLine,
-    NextLine is CurrentLine - 1,
-    replace_on_board(TargetColumn-TargetLine, NextLine, Element, Board, NewBoard).
-
-replace_on_board(TargetColumn-TargetLine, TargetLine, Element, [Line | Board], [NewLine | Board]) :-
-    replace_on_line(TargetColumn, 1, Element, Line, NewLine).
-
-/**
- * Replaces the TargetColumn cell value for Element
- * Helper Function for replace_on_board/5
- */
-% replace_on_line(+TargetColumn, +CurrentColumn, +Element, +Line, -NewLine)
-replace_on_line(TargetColumn, CurrentColumn, Element, [Cell | Line],  [Cell | NewLine]) :-
-    CurrentColumn < TargetColumn,
-    NextColumn is CurrentColumn + 1,
-    replace_on_line(TargetColumn, NextColumn, Element, Line, NewLine).
-
-replace_on_line(TargetColumn, TargetColumn, Element, [_ | Line], [Element | Line]).
 
 /**
  --------------------------------------------------------------------------------
@@ -106,8 +56,8 @@ replace_on_line(TargetColumn, TargetColumn, Element, [_ | Line], [Element | Line
  */
 % game_over(+GameState, -Winner)
 game_over(Dimensions-Board-PlayerOnMove, Winner) :-
-    find_path_blue_blue(Dimensions-Board, BluePath),
-    find_path_red_red(Dimensions-Board, RedPath),
+    find_path_left_right(Dimensions-Board, BluePath),
+    find_path_up_down(Dimensions-Board, RedPath),
     find_winner(BluePath-RedPath, PlayerOnMove, Winner).
 
 /**
@@ -119,31 +69,3 @@ find_winner(1-0, _, -1).
 find_winner(0-1, _,  1).
 find_winner(1-1, Player, Winner) :-
     Winner is -Player.
-
-/**
- * Checks if a path exists between the red edges (up and down)
- * BoardInfo = Dimensions-Board
- * Return Values:
- *          0 - no path
- *          1 - path
- */
-% find_path_red_red(+BoardInfo, -Path)
-find_path_red_red(Dimensions-Board, 1) :-
-    find_red_paths(Dimensions-Board).
-find_path_red_red(Dimensions-Board, 0).
-
-find_red_paths(Dimensions-Board).
-
-/**
- * Checks if a path exists between the blue edges (left and right)
- * BoardInfo = Dimensions-Board
- * Return Values:
- *          0 - no path
- *          1 - path
- */
-% find_path_blue_blue(+BoardInfo, -Path)
-find_path_blue_blue(Dimensions-Board, 1) :-
-    find_blue_paths(Dimensions-Board).
-find_path_blue_blue(Dimensions-Board, 0).
-
-find_blue_paths(Dimensions-Board).
--- utils.pl
+++ utils.pl
@@ -1,4 +1,10 @@
 /**
+ --------------------------------------------------------------------------------
+ --------------------              Player Symbol             --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
  * Player Symbol
  * Red  Player (  1) is X
  * Blue Player (- 1) is O
@@ -6,3 +12,167 @@
 % player_symbol(+Player, -Symbol)
 player_symbol(  1, 'X').
 player_symbol(- 1, 'O').
+
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------                 Replace                --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
+ * Replace the Cell Value in Board for Element
+ * BoardInfo = Dimensions-Board
+ * In the board, Column goes from      1     to Dimensions
+ *           and  Line  goes from Dimensions to      1
+ */
+% replace(+Cell, +Element, +BoardInfo, -NewBoard)
+replace(Column-Line, Element, Dimensions-Board, NewBoard) :-
+    replace_on_board(Column-Line, Dimensions, Element, Board, NewBoard).
+
+/**
+ * Replaces the TargetCell value for Element
+ * Helper Function for replace/4
+ */
+% replace_on_board(+TargetCell, +CurrentLine, +Element, +Board, -NewBoard)
+replace_on_board(TargetColumn-TargetLine, CurrentLine, Element, [Line | Board], [Line | NewBoard]) :-
+    CurrentLine > TargetLine,
+    NextLine is CurrentLine - 1,
+    replace_on_board(TargetColumn-TargetLine, NextLine, Element, Board, NewBoard).
+
+replace_on_board(TargetColumn-TargetLine, TargetLine, Element, [Line | Board], [NewLine | Board]) :-
+    replace_on_line(TargetColumn, 1, Element, Line, NewLine).
+
+/**
+ * Replaces the TargetColumn cell value for Element
+ * Helper Function for replace_on_board/5
+ */
+% replace_on_line(+TargetColumn, +CurrentColumn, +Element, +Line, -NewLine)
+replace_on_line(TargetColumn, CurrentColumn, Element, [Cell | Line],  [Cell | NewLine]) :-
+    CurrentColumn < TargetColumn,
+    NextColumn is CurrentColumn + 1,
+    replace_on_line(TargetColumn, NextColumn, Element, Line, NewLine).
+
+replace_on_line(TargetColumn, TargetColumn, Element, [_ | Line], [Element | Line]).
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------        Position Inside the Board       --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
+ * Verifies if the Position is Inside the Board
+ */
+% inside_board(+Position, +Dimensions)
+inside_board(Column-Line, Dimensions) :-
+    Column > 0,
+    Column =< Dimensions,
+    Line > 0,
+    Line =< Dimensions.
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------               Board Cell               --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
+ * Obtains the Element in the Cell
+ * BoardInfo = Dimensions-Board
+ *  Line  goes from Dimensions to      1
+ * Column goes from      1     to Dimensions
+ */
+% board_cell(+Position, +BoardInfo, -Element)
+board_cell(Column-Line, Dimensions-Board, Element) :-
+    inside_board(Column-Line),
+    LineNumber is Dimensions - Line + 1,
+    nth1(LineNumber, Board, BoardLine),
+    nth1(Column, BoardLine, Element).
+
+board_cell(_, _, '-').
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------            Adjacent Position           --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
+ * Calculates the Position of the Adjacent Piece
+ * Adjacency:
+ *      Up    : u
+ *      Down  : d
+ *      Left  : l
+ *      Right : r
+ */
+% adjacent_position(+Cell, +Adjacency, -NewCell)
+adjacent_position(Column-Line, u, Column-NextLine) :-
+    NextLine is Line + 1.
+
+adjacent_position(Column-Line, d, Column-NextLine) :-
+    NextLine is Line - 1.
+
+adjacent_position(Column-Line, l, NextColumn-Line) :-
+    NextColumn is Column - 1.
+
+adjacent_position(Column-Line, r, NextColumn-Line) :-
+    NextColumn is Column + 1.
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------              Adjacent Cell             --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
+ * Obtains the Adjacent Element
+ * BoardInfo = Dimensions-Board
+ * Element:
+ *      ' ' - empty
+ *      '-' - outside of board
+ */
+% adjacent_cell(+Position, +Adjacency, +BoardInfo, -Element)
+adjacent_cell(Position, Adjacency, BoardInfo, Element) :-
+    adjacent_position(Position, Adjacency, NextPosition),
+    board_cell(NextPosition, BoardInfo, Element).
+
+/**
+ --------------------------------------------------------------------------------
+ --------------------               Path Finder              --------------------
+ --------------------------------------------------------------------------------
+**/
+
+/**
+ * Checks if a path exists between the red edges (up and down)
+ * BoardInfo = Dimensions-Board
+ * Return Values:
+ *          0 - no path
+ *          1 - path
+ */
+% find_path_red_red(+BoardInfo, -Path)
+find_path_red_red(Dimensions-Board, 1) :-
+    find_path_up_down(Dimensions-Board).
+
+/**
+ * Tries to find a path between the edges UP and DOWN
+ */
+% find_path_up_down(+BoardInfo)
+find_path_up_down(_).
+
+/**
+ * Checks if a path exists between the blue edges (left and right)
+ * BoardInfo = Dimensions-Board
+ * Return Values:
+ *          0 - no path
+ *          1 - path
+ */
+% find_path_blue_blue(+BoardInfo, -Path)
+find_path_blue_blue(Dimensions-Board, 1) :-
+    find_path_left_right(Dimensions-Board).
+
+/**
+ * Tries to find a path between the edges LEFT and RIGHT
+ */
+% find_path_left_right(+BoardInfo)
+find_path_left_right(_).
