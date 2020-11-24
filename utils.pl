a83506392279604e2598f8d465a89411e3f7290b
Clara Martins <up201806528@fe.up.pt>
2020-11-24 08:33:21 +0000

    Continuing Path Finder

--- utils.pl
+++ utils.pl
@@ -186,6 +186,31 @@ get_pieces_from_player_on_line(_, [], Pieces, Pieces).
 **/
 
 /**
+ * Creates a visited board
+ *          0 - not visited
+ *          1 - visited
+ */
+% create_visited_board(+Line, +Dimensions, -Visited)
+create_visited_board(Line, Dimensions, [BoardLine | Visited]) :-
+    Line > 0,
+    create_visited_line(Dimensions, BoardLine),
+    NextLine is Line - 1,
+    create_visited_board(NextLine, Dimensions, Visited).
+
+create_visited_board(0, _, []).
+
+/**
+ * Creates a visited line
+ */
+% create_visited_line(+Column, -VisitedLine)
+create_visited_line(Column, [0 | Line]) :-
+    Column > 0,
+    NextColumn is Column - 1,
+    create_visited_line(NextColumn, Line).
+
+create_visited_line(0, []).
+
+/**
  * Checks if a path exists between the red edges (up and down)
  * BoardInfo = Dimensions-Board
  * Return Values:
@@ -194,13 +219,29 @@ get_pieces_from_player_on_line(_, [], Pieces, Pieces).
  */
 % find_path_red_red(+BoardInfo, -Path)
 find_path_red_red(Dimensions-Board, 1) :-
-    find_path_up_down(Dimensions-Board).
+    create_visited_board(Dimensions, Dimensions, Visited),
+    find_red_path(1, Dimensions, Board, Visited).
+
+find_path_red_red(_, 0).
+
+/**
+ * Finds a Path between Red Edges
+ */
+% find_red_path(+Column, +Dimensions, +Board, +Visited)
+find_red_path(Column, Dimensions, Board, Visited) :-
+    Column =< Dimensions,
+    find_path_up_down(Column-Dimensions, Dimensions, Board, Visited).
+
+find_red_path(Column, Dimensions, Board, Visited) :-
+    NextColumn is Column + 1,
+    find_red_path(NextColumn, Dimensions, Board, Visited).
 
 /**
  * Tries to find a path between the edges UP and DOWN
  */
-% find_path_up_down(+BoardInfo)
-find_path_up_down(_).
+% find_path_up_down(+Cell, +Dimensions, +Board, +Visited)
+find_path_up_down(Cell, Dimensions, Board, Visited).
+find_path_up_down(_-1, _, _, _).
 
 /**
  * Checks if a path exists between the blue edges (left and right)
@@ -211,10 +252,27 @@ find_path_up_down(_).
  */
 % find_path_blue_blue(+BoardInfo, -Path)
 find_path_blue_blue(Dimensions-Board, 1) :-
-    find_path_left_right(Dimensions-Board).
+    create_visited_board(Dimensions, Dimensions, Visited),
+    find_blue_path(1, Dimensions, Board, Visited).
+
+find_path_blue_blue(_, 0).
+
+/**
+ * Finds a Path between Blue Edges
+ */
+% find_blue_path(+BoardInfo)
+find_blue_path(Line, Dimensions, Board, Visited) :-
+    Line =< Dimensions,
+    find_path_left_right(1-Line, Dimensions, Board, Visited).
+
+find_blue_path(Column, Dimensions, Board, Visited) :-
+    NextLine is Line + 1,
+    find_blue_path(NextLine, Dimensions, Board, Visited).
+
 
 /**
  * Tries to find a path between the edges LEFT and RIGHT
  */
-% find_path_left_right(+BoardInfo)
-find_path_left_right(_).
+% find_path_left_right(+Cell, +Dimensions, +Board, +Visited)
+find_path_left_right(Cell, Dimensions, Board, Visited).
+find_path_left_right(Dimensions-_, Dimensions, _, _).
