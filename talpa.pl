93ef6fbba82d81a77d1dac258037d0063a769108
Clara Martins <50105043+CAMartins@users.noreply.github.com>
2020-11-27 16:44:06 +0000

    Testing

--- talpa.pl
+++ talpa.pl
@@ -2,3 +2,31 @@
 :- ensure_loaded('menu.pl').
 
 play :- menu(0).
+
+/*
+init(D, D-[['O','X','O','X','O','X','O','X'],
+              ['X','O','X','O','X','O','X','O'],
+              ['O','X','O','X','O','X','O','X'],
+              ['X','O','X','O','X','O','X','O'],
+              ['O','X','O','X','O','X','O','X'],
+              ['X','O','X','O','X','O','X','O'],
+              ['O','X','O','X','O','X','O','X'],
+              ['X','O','X','O','X','O','X','O']]-1).
+
+play :- init(8, GameState),
+        game2(GameState, 0-0).
+
+play :- init(8, GameState),
+        valid_moves(GameState, _, Moves),
+        write(Moves).
+game2(GameState, Players) :-
+    display_game(GameState, _),
+    write('1\n'),
+    choose_player_level(GameState, Players, Level),
+    write('2\n'),
+    choose_move(GameState, _, Level, Move),
+    write('3\n'),
+    move(GameState, Move, NewGameState),
+    write('4\n'),
+    game2(NewGameState, Players).
+*/
