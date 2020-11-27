6dcec33208f9e117e687109973aca6ccc5b234b1
Clara Martins <50105043+CAMartins@users.noreply.github.com>
2020-11-27 18:40:52 +0000

    Everything except value and choose_ai_greedy_move is working

--- talpa.pl
+++ talpa.pl
@@ -1,8 +1,9 @@
 % :- ensure_loaded('intermediate_delivery.pl').
 :- ensure_loaded('menu.pl').
 
-% play :- menu(0).
+play :- menu(0).
 
+/*
 init(6, 6-[   ['O','X','O','X','O','X'],
               ['X','O','X','O','X','O'],
               ['O','X','O','X','O','X'],
@@ -31,23 +32,5 @@ init(10, 10-[ ['O','X','O','X','O','X','O','X','O','X'],
               ['X','O','X','O','X','O','X','O','X','O']]-1).
 
 play :- init(6, GameState),
-        game(GameState, 0-0).
+        game(GameState, 1-1).*/
 
-
-/*play :- init(8, GameState),
-        game2(GameState, 0-0).
-
-play :- init(8, GameState),
-        valid_moves(GameState, _, Moves),
-        write(Moves).
-game2(GameState, Players) :-
-    display_game(GameState, _),
-    write('1\n'),
-    choose_player_level(GameState, Players, Level),
-    write('2\n'),
-    choose_move(GameState, _, Level, Move),
-    write('3\n'),
-    move(GameState, Move, NewGameState),
-    write('4\n'),
-    game2(NewGameState, Players).
-*/
